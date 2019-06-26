7d78ec2800af3cf25a230a53a40811e3604aca4c
lidawei <lidawei@iciyun.com>
2019-06-26 10:20:56 +0800

    fitst

---
+++ src/controller/HelloController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:ldw
+ * @Description
+ * @Date ：Create in 10:20 2019/6/26
+ * @Modified By :
+ */
+@RestController
+public class HelloController {
+
+
+}
