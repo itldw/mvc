78843b0048be198670e8924b9ebf7129f64c68c7
lidawei <lidawei@iciyun.com>
2019-06-27 12:14:56 +0800

    f

--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -1,16 +1,21 @@
 package controller;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author:ldw
  * @Description
- * @Date ：Create in 10:20 2019/6/26
+ * @Date ：Create in 17:20 2019/6/26
  * @Modified By :
  */
-@RestController
+@RestController("/")
 public class HelloController {
 
-
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return  "hello";
+    }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
   User: ciyun
-  Date: 2019/6/25
-  Time: 18:00
+  Date: 2019/6/26
+  Time: 10:57
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
